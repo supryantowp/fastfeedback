b4daaf992ebc13a5f9e1cb7daff6139ad121749f
supryantowp <supryantowp21@gmail.com>
2020-12-20 22:11:22 +0700

    chore: fix env private key to vercel

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -6,7 +6,7 @@ if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
     }),
     databaseURL: 'https://fast-feedback-a41d5-default-rtdb.firebaseio.com/'
